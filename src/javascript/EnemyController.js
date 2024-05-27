8e3d5fb19cd094711b0dc495d9898f29ec7e5009
Danie1zika <danielbrandao03420@gmail.com>
2024-05-27 10:12:46 -0400

    add

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -113,4 +113,15 @@ drawEnemies(ctx) {
 }
 
 happy = () => {};
+
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex) => {
+           this.enemyRows[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) => {
+                if(enemyNumber > 0) {
+                    this.enemyRows[rowIndex].push(new Enemy(enemyIndex * 50, rowIndex * 35, enemyNumber));
+                }
+        });
+    })
+}
 }
